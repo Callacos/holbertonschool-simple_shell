9cb1ae89461e00c7a696a806d0409f39941b53ca
Callacos <callacos74@gmail.com>
2025-01-07 17:17:33 +0100

    Refactor command input handling and exit verification in simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -22,43 +22,45 @@ int i;
 
 while (1)
 {
-    printf("callayass_shell$ ");
-    if (fgets(command, sizeof(command), stdin) == NULL)
-        break;
+printf("callayass_shell$ ");
+if (fgets(command, sizeof(command), stdin) == NULL)
+{
+	break;
+}
 
-    command[strcspn(command, "\n")] = '\0';
+command[strcspn(command, "\n")] = '\0';
 
-    i = 0;
-    args[i] = strtok(command, " \t");
-    while (args[i] != NULL && i < 9)
-    {
-        args[++i] = strtok(NULL, " \t");
-    }
-    args[i] = NULL;
+i = 0;
+args[i] = strtok(command, " \t");
+while (args[i] != NULL && i < 9)
+{
+	args[++i] = strtok(NULL, " \t");
+}
+args[i] = NULL;
 
-    /* Vérification de la commande exit */
-    if (args[0] != NULL && strcmp(args[0], "exit") == 0)
-    {
-        custom_exit(args);
-    }
+/* Vérification de la commande exit */
+if (args[0] != NULL && strcmp(args[0], "exit") == 0)
+{
+	custom_exit(args);
+}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("Erreur");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid > 0)
-    {
-        wait(NULL);
-    }
-    else
-    {
-        perror("Erreur de fork");
-    }
+pid = fork();
+if (pid == 0)
+{
+if (execvp(args[0], args) == -1)
+{
+	perror("Erreur");
+	exit(EXIT_FAILURE);
+}
+}
+else if (pid > 0)
+{
+	wait(NULL);
+}
+else
+{
+	perror("Erreur de fork");
+}
 }
 
 return (0);
